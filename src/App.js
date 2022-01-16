88d88886ad785e36a110d452ccff67523c67bdc7
Dmitri Golota <dmitri.golota@gmail.com>
2022-01-15 17:12:24 -0800

    Added packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import Header from './Header';
+import $ from 'jquery';
 
 function App() {
+  $.get('localhost:2000/DM3H7P',  // url
+      function (data, textStatus, jqXHR) {  // success callback
+          alert('status: ' + textStatus + ', data:' + data);
+  });
   return (
     <div className="App">
       <Header/>
