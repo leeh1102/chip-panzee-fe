7c1bb2fc01a3b26b51b57349e66f68d93be3826a
Subin_Moon <joy120594@gmail.com>
2022-01-15 17:06:55 -0800

    Modify the font file names, Update App.css, and delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './fonts/BobbyJonesSoft.otf';
+import './fonts/Kollektif.ttf';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
