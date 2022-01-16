0feb9afb4ccb3b9c7070f2d1f9883c2d0362f546
Subin_Moon <joy120594@gmail.com>
2022-01-16 00:14:03 -0800

    Complete the webform

--- src/components/PostCreateComponent/PostCreateComponent.js
+++ src/components/PostCreateComponent/PostCreateComponent.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 import styles from './PostCreateComponent.module.css';
 
 import Box from '@mui/material/Box';
+import { Grid } from '@mui/material';
+import { TextField } from '@mui/material';
+import { useFormControl } from '@mui/material/FormControl';
+
 import Input from '@mui/material/Input';
 import { Container, IconButton, Typography } from '@mui/material';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import Button from '@mui/material/Button';
+
+//TODO Function to detect either one of the e-transfer information is filled; if both are not filled, the form can't be submitted.
 
 export default function PostCreateComponent() {
   const handleSubmit = (event) => {
@@ -20,7 +26,7 @@ export default function PostCreateComponent() {
 
   return (
 
-    <Container component="main" maxWidth="xs">
+    <Container maxWidth="xs">
       <Box
         sx={{
           marginTop: 8,
@@ -37,43 +43,135 @@ export default function PostCreateComponent() {
 
         <Box
           component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="userName"
+                required
+                fullWidth
+                id="userName"
+                label="YOUR NAME"
+                autoFocus
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="passcode"
+                label="PASSCODE"
+                name="passcode"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="postTitle"
+                label="POST TITLE"
+                name="postTitle"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="fullAmount"
+                required
+                fullWidth
+                id="fullAmount"
+                label="FULL AMOUNT"
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                autoFocus
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="numOfPals"
+                label="# OF PALS"
+                name="numOfPals"
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="description"
+                label="DESCRIPTION"
+                placeholder="Description"
+                name="description"
+                multiline
+                fullWidth
+                rows={4}
+                variant="filled"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="detailURL"
+                label="PRODUCT DETAIL WEBSITE URL"
+                name="detailURL"
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} mt={2}>
+              <p>E-TRANSFER INFORMATION</p>
+              <p>*Please provide one of the information below</p>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="phoneNumber"
+                fullWidth
+                id="phoneNumber"
+                label="PHONE #"
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                autoFocus
+                variant="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="email"
+                label="EMAIL"
+                name="email"
+                variant="standard"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            className={styles.submitButton}
+            type="submit"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            READY TO CHIP IN?
+          </Button>
         </Box>
       </Box>
 
     </Container>
 
-    /* <div className={styles.PostCreateComponent}>
-  
-      <div>
-        <h1 className={styles.PostCreateComponentTitle}>
-          WELCOM TO CHIPPIN
-        </h1>
-  
-        <p className={styles.PostCreateDescription}>The most convenient and fastest way of chipping in with your pals for any gifts 🎁 💰</p>
-  
-        <hr className={styles.PostCreateTitleHr} />
-  
-      </div> */
+    /* <Box
+      className={styles.CreatePostForm}
+      component="form" sx={{ mt: 3 }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}>
+      <div className={styles.uploadFile}>
+        <Input accept="image/*" className={styles.uploadFileIcon} type="file" />
+        <IconButton color="primary" className={styles.photoCameraIcon} aria-label="upload picture" component="span">
+          <PhotoCameraIcon />
+        </IconButton>
+      </div>
+    </Box> */
 
-    /* <form className={styles.CreatePostForm}> */
-    // <Box
-    //   className={styles.CreatePostForm}
-    //   component="form" sx={{ mt: 3 }}
-    //   noValidate
-    //   autoComplete="off"
-    //   onSubmit={handleSubmit}>
-    //   <div className={styles.uploadFile}>
-    //     <Input accept="image/*" className={styles.uploadFileIcon} type="file" />
-    //     <IconButton color="primary" className={styles.photoCameraIcon} aria-label="upload picture" component="span">
-    //       <PhotoCameraIcon />
-    //     </IconButton>
-    //   </div>
-    //   <p>Hi</p>
-    // </Box>
-    /* </form> */
-
-    // </div>
   );
 }
 
