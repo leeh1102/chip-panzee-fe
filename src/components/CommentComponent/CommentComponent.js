876b6cd083da1bab0740d4ecc56c1a1ccf44b2ab
Heqian Liu <lhq@bu.edu>
2022-01-15 18:15:09 -0800

    add component

---
+++ src/components/CommentComponent/CommentComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CommentComponent.module.css';
+
+const CommentComponent = () => (
+  <div className={styles.CommentComponent}>
+    CommentComponent Component
+  </div>
+);
+
+CommentComponent.propTypes = {};
+
+CommentComponent.defaultProps = {};
+
+export default CommentComponent;
