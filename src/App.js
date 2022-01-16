9ed50c4fc8d49614cab921deeb5a852836aa9390
Dmitri Golota <dmitri.golota@gmail.com>
2022-01-16 04:52:02 -0800

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -20,15 +20,8 @@ function App() {
   }
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <PostDetailComponent post={post} setPost={setPost}/>
-       */}
-      <PostCreateComponent />
-
-=======
       {/* <PostDetailComponent post={post} setPost={setPost}/> */}
       <PostDetailComponent/>
->>>>>>> 44c34ee6e6582eb26d1a46611aed27360c238764
     </div>
   );
 }
